e2700730e0b80dfc3ee9ee79cb8d539679fc282f
Misla Wislaine <mislawislaine12@gmail.com>
2023-06-17 16:28:02 -0300

    Initial commit

---
+++ aula03/exercicios/index.js
@@ -0,0 +1,6 @@
+console.log('Meu nome é "MISLA".', 'Estou aprendendo JavaScript às', 15, 'da tarde.');
+
+//correção 
+console.log('Meu nome é "NOME". Estou aprendendo JavaScript às', 10, 'da manhã.');
+
+console.log('Este', 'É', 'Um', 1000);
---
+++ aula03/index.js
@@ -0,0 +1,6 @@
+console.log('Misla Wislaine'); // String = Texto
+console.log("Misla Wislaine"); // Number = número
+console.log(`Misla Wislaine`);
+
+console.log(123456);
+console.log(35, 15.85, "Misla Wislaine");
