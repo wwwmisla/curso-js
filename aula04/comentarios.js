38f0cae9fe5fea03e85fa7e1dca46cf6ca899a2f
Misla Wislaine <mislawislaine12@gmail.com>
2023-06-17 17:16:12 -0300

    aula04

---
+++ aula04/comentarios.js
@@ -0,0 +1,13 @@
+// E escrever um comentário
+console.log('Olá mundo!'); // Aqui tem outro comentário
+console.log('Linha 1');
+// Comentário de linha 
+
+/*
+ * Comentário em bloco
+*/
+
+//console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+// Vai ser ignorado
