0759b37bb0de7c8ae350634083a1f74c9f21d917
Misla Wislaine <mislawislaine12@gmail.com>
2024-01-04 17:14:46 -0300

    funções (básico)

--- aula20/index.js
+++ aula20/index.js
@@ -1,8 +1,26 @@
-function saudacao(nome){
-    //console.log(`Bom dia ${nome}!`);
-
-    return `Bom dia ${nome}!`;
+//function soma(x, y){
+function soma(x = 0, y = 0){
+    const resultado = x + y;
+    return resultado;
 }
 
-const variavel = saudacao('Luiz');
-console.log(variavel);
+console.log(soma(2, 2));
+
+const resultado = soma(5, 5);
+console.log(resultado);
+
+console.log(soma());
+
+const raiz = function (n){
+    return n ** 0.5;
+};
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
+
+const raiz2 = n => n ** 0.5;
+
+console.log(raiz2(9));
+console.log(raiz2(16));
+console.log(raiz2(25));
