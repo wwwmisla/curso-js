84bf002967c1313c0b14615e04b8cb31485e7358
Misla Wislaine <mislawislaine12@gmail.com>
2024-01-04 20:00:23 -0300

    objetos com function

--- aula21/index.js
+++ aula21/index.js
@@ -1,22 +1,13 @@
-const nome1 = 'Luiz';
-const sobrenome1 = 'Miranda';
-const idade1 = 25;
+function criaPessoa(nome, sobrenome, idade) {
+    // return {
+    //     nome: nome,
+    //     sobrenome: sobrenome,
+    //     idade: idade
+    // };
 
-const nome2 = 'Maria';
-const sobrenome2 = 'Fernanda';
-const idade2 = 27;
+    return {nome, sobrenome, idade}
+}
 
-const pessoa1 = {
-    nome: 'Luiz',
-    sobrenome: 'Miranda',
-    idade: 25
-};
-
-const pessoa2 = {
-    nome: 'Maria',
-    sobrenome: 'Fernanda',
-    idade: 27
-};
-
-console.log(pessoa1.nome);
-console.log(pessoa2.nome);
+const pessoa1 = criaPessoa('Luiz', 'Miranda', 25);
+const pessoa2 = criaPessoa('Maria', 'Fernanda', 27);
+console.log(pessoa1.nome, pessoa2.nome);
