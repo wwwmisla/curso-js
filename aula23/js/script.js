219a2fbe311e4180976ed978ec3753c67d3a688b
Misla Wislaine <mislawislaine12@gmail.com>
2024-01-05 19:32:43 -0300

    exercícios função, array e objeto

--- aula23/js/script.js
+++ aula23/js/script.js
@@ -1,11 +1,30 @@
 function meuEscopo () {
     const form = document.querySelector('.form');
+    const resultado = document.querySelector('.resultado');
 
-    form.onsubmit = function (evento) {
+    const pessoas = [];
+
+    function recebeEventoForm(evento) {
         evento.preventDefault();
-        alert(1);
-        console.log('Foi enviado.');
-    };
+
+        const nome = form.querySelector('.nome');
+        const sobrenome = form.querySelector('.sobrenome');
+        const peso = form.querySelector('.peso');
+        const altura = form.querySelector('.altura');
+
+        pessoas.push({
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value
+        });
+
+        console.log(pessoas);
+
+        resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ${peso.value} ${altura.value}</p>`;
+    }
+
+    form.addEventListener('submit', recebeEventoForm);
 
 }
 
