3f583eecb1c2d5f8c198631739f55ce8737fa12b
Misla Wislaine <mislawislaine12@gmail.com>
2024-01-06 19:33:44 -0300

    Operadores de comparação

---
+++ Seção 3 - JavaScript - Lógica de Programação/aula01/index.js
@@ -0,0 +1,13 @@
+/*
+Operadores de comparação
+> maior que
+>= maior que ou igual a
+< menor que
+<= menos que ou igual a 
+== igualdade (valor) ************
+=== igualdade estrita (valor e tipo)
+!= diferente (valor) ************
+!== diferente estrito (valor e tipo)
+*/
+const comp = 10 === 10;
+console.log(comp);
