5d2bf10b08d3eaa1273838fc7aff2784c786a9c9
Misla Wislaine <mislawislaine12@gmail.com>
2024-01-07 20:12:42 -0300

    if, else if e else (1)

---
+++ Seção 3 - JavaScript - Lógica de Programação/aula04/index.js
@@ -0,0 +1,33 @@
+/*
+Entre 0 e 11 - Bom dia
+Entre 12 e 17 - Boa tarde
+Entre 18 e 23 - Boa noite
+*/
+
+/*
+If pode ser usado sozinho
+Sempre que utilizo a palavra else, preciso de um if antes
+Eu posso ter vários else ifs na checagem 
+Só posso ter um else na checagem
+Podemos usar condiçoes sem else if, utilizando apenas if e else
+*/
+
+const hora = 9;
+
+if (hora >= 0 && hora <= 11) {
+    console.log('Bom dia!');
+} else if (hora >= 12 && hora <= 17) {
+    console.log('Boa tarde!');
+} else if (hora >= 18 && hora <= 23) {
+    console.log('Boa noite!');
+} else {
+    console.log('Olá!')
+}
+
+const tenhoGrana = false;
+
+if (tenhoGrana) {
+    console.log('Vou sair de casa!');
+} else {
+    console.log('Não vou sair de casa!');
+}
