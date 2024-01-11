f1b46c000df2da825bcf4a82ad6c57cc496e11db
Misla Wislaine <mislawislaine12@gmail.com>
2024-01-10 23:24:57 -0300

    Objeto Date

--- Seção 3 - JavaScript - Lógica de Programação/aula08/index.js
+++ Seção 3 - JavaScript - Lógica de Programação/aula08/index.js
@@ -1,14 +1,20 @@
-const data = new Date('2021-04-24 16:54:48.100');
-console.log('Dia', data.getDate());
-console.log('Mês', data.getMonth() + 1); //0-11 - mês começa do zero
-console.log('Ano', data.getFullYear());
-console.log('Hora', data.getHours());
-console.log('Min', data.getMinutes());
-console.log('Seg', data.getSeconds());
-console.log('Ms', data.getMilliseconds());
-console.log('Dia semana', data.getDay()); // 0 - domingo, 6 - sábado
-console.log(data.toString());
+function zeroAEsquerda(num) {
+    return num >= 10 ? num : `0${num}`;
+}
 
-console.log(Date.now());
-const data02 = new Date(1704939194655);
-console.log(data02.toString());
+function formataData(data) {
+    //console.log(data);
+
+    const dia = zeroAEsquerda(data.getDate());
+    const mes = zeroAEsquerda(data.getMonth() + 1);
+    const ano = zeroAEsquerda(data.getFullYear());
+    const hora = zeroAEsquerda(data.getHours());
+    const min = zeroAEsquerda(data.getMinutes());
+    const seg = zeroAEsquerda(data.getSeconds());
+
+    return `${dia}/${mes}/${ano} ${hora}:${min}:${seg}`;
+}
+
+const data = new Date();
+const dataBrasil = formataData(data);
+console.log(dataBrasil);
