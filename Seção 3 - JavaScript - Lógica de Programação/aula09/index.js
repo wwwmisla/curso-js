6d7a122cd0dc31b563ef6005c0d4687ae8e8620b
Misla Wislaine <mislawislaine12@gmail.com>
2024-01-11 17:24:55 -0300

    Switch/Case (2)

--- Seção 3 - JavaScript - Lógica de Programação/aula09/index.js
+++ Seção 3 - JavaScript - Lógica de Programação/aula09/index.js
@@ -1,4 +1,23 @@
-const data = new Date();
+const data = new Date('1987-04-24 00:00:00');
 const diaSemana = data.getDay();
+let diaSemanaTexto;
 
-console.log(diaSemana);
+if (diaSemana === 0){
+    diaSemanaTexto = 'Domingo';
+} else if (diaSemana === 1){
+    diaSemanaTexto = 'Segunda';
+} else if (diaSemana === 2){
+    diaSemanaTexto = 'Terça';
+} else if (diaSemana === 3){
+    diaSemanaTexto = 'Quarta';
+} else if (diaSemana === 4){
+    diaSemanaTexto = 'Quinta';
+} else if (diaSemana === 5){
+    diaSemanaTexto = 'Sexta';
+} else if (diaSemana === 6){
+    diaSemanaTexto = 'Sábado';
+} else {
+    diaSemanaTexto = '';
+}
+
+console.log(diaSemana, diaSemanaTexto);
