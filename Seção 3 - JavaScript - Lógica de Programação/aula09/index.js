18d5c8ddaec3fba198edf50b221f975fc8a5771d
Misla Wislaine <mislawislaine12@gmail.com>
2024-01-11 17:38:13 -0300

    Switch/Case com function (4)

--- Seção 3 - JavaScript - Lógica de Programação/aula09/index.js
+++ Seção 3 - JavaScript - Lógica de Programação/aula09/index.js
@@ -1,32 +1,36 @@
-const data = new Date('1987-04-24 00:00:00');
-const diaSemana = data.getDay();
-let diaSemanaTexto;
+function getDiaSemanaTexto(diaSemana) {
+    let diaSemanaTexto;
 
-switch (diaSemana) {
-    case 0:
-        diaSemanaTexto = 'Domingo';
-        break;
-    case 1:
-        diaSemanaTexto = 'Segunda';
-        break;
-    case 2:
-        diaSemanaTexto = 'Terça';
-        break;
-    case 3:
-        diaSemanaTexto = 'Quarta';
-        break;
-    case 4:
-        diaSemanaTexto = 'Quinta';
-        break;
-    case 5:
-        diaSemanaTexto = 'Sexta';
-        break;
-    case 6:
-        diaSemanaTexto = 'Sábado';
-        break;
-    default:
-        diaSemanaTexto = '';
-        break;
+    switch (diaSemana) {
+        case 0:
+            diaSemanaTexto = 'Domingo';
+            return diaSemanaTexto;
+        case 1:
+            diaSemanaTexto = 'Segunda';
+            return diaSemanaTexto;
+        case 2:
+            diaSemanaTexto = 'Terça';
+            return diaSemanaTexto;
+        case 3:
+            diaSemanaTexto = 'Quarta';
+            return diaSemanaTexto;
+        case 4:
+            diaSemanaTexto = 'Quinta';
+            return diaSemanaTexto;
+        case 5:
+            diaSemanaTexto = 'Sexta';
+            return diaSemanaTexto;
+        case 6:
+            diaSemanaTexto = 'Sábado';
+            return diaSemanaTexto;
+        default:
+            diaSemanaTexto = '';
+            return diaSemanaTexto;
+    }
 }
+
+const data = new Date('1987-04-21 00:00:00');
+const diaSemana = data.getDay();
+const diaSemanaTexto = getDiaSemanaTexto(diaSemana);
 
 console.log(diaSemana, diaSemanaTexto);
