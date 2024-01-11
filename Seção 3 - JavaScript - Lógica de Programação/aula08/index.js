0a224094b60c956ffd1d89b168c14da61da3354d
Misla Wislaine <mislawislaine12@gmail.com>
2024-01-10 23:15:24 -0300

    Objeto Date Base 2

--- Seção 3 - JavaScript - Lógica de Programação/aula08/index.js
+++ Seção 3 - JavaScript - Lógica de Programação/aula08/index.js
@@ -1,13 +1,14 @@
-const data = new Date();
+const data = new Date('2021-04-24 16:54:48.100');
+console.log('Dia', data.getDate());
+console.log('Mês', data.getMonth() + 1); //0-11 - mês começa do zero
+console.log('Ano', data.getFullYear());
+console.log('Hora', data.getHours());
+console.log('Min', data.getMinutes());
+console.log('Seg', data.getSeconds());
+console.log('Ms', data.getMilliseconds());
+console.log('Dia semana', data.getDay()); // 0 - domingo, 6 - sábado
 console.log(data.toString());
 
-const tresHoras = 60 * 60 * 3 * 1000;
-const umDia = 60 * 60 * 24 * 1000;
-const data02 = new Date(0 + tresHoras + umDia); // 01/01/1970 Timestamp unix ou época unix - valor em milisegundos
+console.log(Date.now());
+const data02 = new Date(1704939194655);
 console.log(data02.toString());
-
-const data03 = new Date(2024, 0, 10, 7, 15, 23); // 0-11 mes - a,m,d,h,M,s,ms
-console.log(data03.toString());
-
-const data04 = new Date('2024-01-10 23:04:59'); //formato mais utilizado
-console.log(data04.toString());
