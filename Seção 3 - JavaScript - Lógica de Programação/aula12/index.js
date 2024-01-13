9a27b798242493e7807ee3d2ba3f86f0cc665fb8
Misla Wislaine <mislawislaine12@gmail.com>
2024-01-12 21:46:06 -0300

    base 1

--- Seção 3 - JavaScript - Lógica de Programação/aula12/index.js
+++ Seção 3 - JavaScript - Lógica de Programação/aula12/index.js
@@ -1,8 +1,7 @@
-let a = 'A'; // B
-let b = 'B'; // C
-let c = 'C'; // A
+//               0  1  2  3  4  5  6  7  8
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const primeiroNumero = numeros[0];
+console.log(primeiroNumero);
 
-const numeros = [1, 2, 3];
-[a, b, c] = numeros; // desestruturação
-
-console.log(a, b, c);
+const [num1, num2] = numeros;
+console.log(num1, num2);
