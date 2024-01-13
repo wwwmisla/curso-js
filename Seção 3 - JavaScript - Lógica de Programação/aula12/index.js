d048712844369f57c4249e316bd4791465425438
Misla Wislaine <mislawislaine12@gmail.com>
2024-01-12 21:51:57 -0300

    Numeros

--- Seção 3 - JavaScript - Lógica de Programação/aula12/index.js
+++ Seção 3 - JavaScript - Lógica de Programação/aula12/index.js
@@ -1,5 +1,4 @@
 // ... rest, ... spread
 const numeros = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000];
-const [um, dois, tres, ...resto] = numeros;
-console.log(um, dois, tres);
-console.log(resto);
+const [um, , tres, , cinco] = numeros;
+console.log(um, tres, cinco);
