21794ad26e607ffb9835051a5bf22cdaaf4b22ef
Misla Wislaine <mislawislaine12@gmail.com>
2024-01-12 21:59:12 -0300

    Atribuição via desestruturação (Arrays)

--- Seção 3 - JavaScript - Lógica de Programação/aula12/index.js
+++ Seção 3 - JavaScript - Lógica de Programação/aula12/index.js
@@ -1,4 +1,12 @@
 // ... rest, ... spread
-const numeros = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000];
-const [um, , tres, , cinco] = numeros;
-console.log(um, tres, cinco);
+//                    0             1           2
+//                 0   1   2    0   1  2    0  1  2
+const numeros = [ [1 , 2 , 3], [4 , 5, 6], [7, 8, 9] ];
+console.log(numeros[1][2]);
+
+const [,[,,seis]] = numeros;
+console.log(seis);
+
+const [lista1, lista2, lista3] = numeros;
+console.log(lista3);
+console.log(lista3[2]);
