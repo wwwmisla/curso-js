1342883319ceefe532c0b6a19c631f919ad182c9
Misla Wislaine <mislawislaine12@gmail.com>
2024-01-12 21:50:30 -0300

    ... rest

--- Seção 3 - JavaScript - Lógica de Programação/aula12/index.js
+++ Seção 3 - JavaScript - Lógica de Programação/aula12/index.js
@@ -1,7 +1,5 @@
-//               0  1  2  3  4  5  6  7  8
-const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const primeiroNumero = numeros[0];
-console.log(primeiroNumero);
-
-const [num1, num2] = numeros;
-console.log(num1, num2);
+// ... rest, ... spread
+const numeros = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000];
+const [um, dois, tres, ...resto] = numeros;
+console.log(um, dois, tres);
+console.log(resto);
