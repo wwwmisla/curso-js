76fc5b36c300148fe068c6a0e5c21153b416331b
Misla Wislaine <mislawislaine12@gmail.com>
2024-01-17 17:25:06 -0300

    Tratando e lançando erros (try, catch, throw, finally)

--- Seção 3 - JavaScript - Lógica de Programação/aula26/index.js
+++ Seção 3 - JavaScript - Lógica de Programação/aula26/index.js
@@ -1,21 +1,26 @@
-try {
-    // É executada quando não há erros
-    // console.log(a);
-    console.log('Abri um arquivo');
-    console.log('Manipulei o arquivo e gerou erro');
-    console.log('Fechei o arquivo');
-
-    try {
-        console.log(a);
-    } catch (e) {
-        console.log('Deu erro');
-    } finally {
-        console.log('Também sou finally');
+function retornaHora (data) {
+    if (data && !(data instanceof Date)) {
+        throw new TypeError ('Esperando instância de Date.');
     }
-} catch (e) {
-    // É executada quando há erros
-    console.log('Tratando o erro')
+
+    if (!data) {
+        data = new Date();
+    }
+
+    return data.toLocaleTimeString('pt-BR', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false
+    });
+}
+
+try {
+    const data = new Date('01-01-1970 12:58:12');
+    const hora = retornaHora(data);
+    console.log(hora);
+} catch(e) {
+    console.log('Tratar erro.', e);
 } finally {
-    // Sempre 
-    console.log('FINALLY: Eu sempre sou executado');
+    console.log('Tenha um bom dia.')
 }
