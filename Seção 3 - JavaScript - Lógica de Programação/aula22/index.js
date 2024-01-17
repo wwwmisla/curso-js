eece7b18ead13dc12d60be04179dc28327e8aa4e
Misla Wislaine <mislawislaine12@gmail.com>
2024-01-16 22:45:45 -0300

    Exercícios

---
+++ Seção 3 - JavaScript - Lógica de Programação/aula22/index.js
@@ -0,0 +1,11 @@
+// function max (x,y) {
+//     return x > y ? x : y;
+// }
+
+// const max = (x, y) => {
+//     return x > y ? x : y;
+// };
+
+const max = (x, y) => x > y ? x : y;
+    
+console.log(max(100,10));
