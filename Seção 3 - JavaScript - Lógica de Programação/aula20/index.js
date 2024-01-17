b01154305f40fec375b543dcaedf38668882983f
Misla Wislaine <mislawislaine12@gmail.com>
2024-01-16 22:20:40 -0300

    while e do while

--- Seção 3 - JavaScript - Lógica de Programação/aula20/index.js
+++ Seção 3 - JavaScript - Lógica de Programação/aula20/index.js
@@ -5,13 +5,16 @@ function random(min, max) {
 
 const min = 1;
 const max = 50;
+// let rand = random(min, max);
 let rand = 10;
 
 while (rand !== 10) {
+    // rand = random(min, max);
     console.log(rand);
 }
 console.log('######');
 
 do {
+    // rand = random(min, max);
     console.log(rand);
 } while (rand !== 10);
