365c2fc279d2fe210b1f68a46c8ebd5a8c31451c
Misla Wislaine <mislawislaine12@gmail.com>
2024-01-16 22:35:36 -0300

    break

--- Seção 3 - JavaScript - Lógica de Programação/aula21/index.js
+++ Seção 3 - JavaScript - Lógica de Programação/aula21/index.js
@@ -1,3 +1,5 @@
+// Continue continua para a próxima iteração
+// Break sai do laço
 const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 // for (let numero of numeros) {
@@ -12,4 +14,9 @@ for (let numero of numeros) {
     }
 
     console.log(numero);
+
+    if(numero === 7) {
+        console.log('Vai parar aqui, pois o 7 foi encontrado!');
+        break;
+    }
 }
